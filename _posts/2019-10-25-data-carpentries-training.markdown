e97817970fbb87a333044fd037a094b845547272
Breanna Sipley <breanna.sipley@gmail.com>
2019-11-12 15:56:05 -0800

    adjusting blog

--- _posts/2019-10-25-data-carpentries-training.markdown
+++ _posts/2019-10-25-data-carpentries-training.markdown
@@ -4,4 +4,6 @@ title:  "Data Carpentries training"
 date:   2019-10-25 20:15:59 -0800
 categories: jekyll update
 ---
+16-hour pedagogical training down!
+
 I just completed the two-day instructor training through The Carpentries alongside a few other colleages from Bioinformatics and Computational Biology. It was so refreshing to talk pedagogy. I'm excited about helping the University of Idaho host a few Software and Data Carpentries workshops in the Spring to help empower others to utilize programming and other computational tools to increase their research productivity. Be on the lookout for more info!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,12 +10,12 @@ var store = [{
         "url": "http://localhost:4000/jekyll/update/RADseq-data-back/",
         "teaser":"http://localhost:4000/assets/images/paradise-ridge.png"},{
         "title": "Moving to Moscow!",
-        "excerpt":"Moscow, Idaho—not Moscow, Russia. I’m excited to announce that I’ve officially accepted the University of Idaho’s offer of admission into their Bioinformatics and Computational Biology PhD program! This Florida girl will be trading the warmth and humidity of the Southeast for the drier, snowier weather home to the rolling hills...","categories": ["jekyll","update"],
+        "excerpt":"Moscow, Idaho—not Moscow, Russia. I’ve officially accepted the University of Idaho’s offer of admission into their Bioinformatics and Computational Biology PhD program! This Florida girl will be trading the warmth and humidity of the Southeast for the drier, snowier weather home to the rolling hills of the Palouse in Northern...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/moving-to-moscow/",
         "teaser":"http://localhost:4000/assets/images/paradise-ridge.png"},{
         "title": "Successfully defended my MS!",
-        "excerpt":"I passed my MS defense! I feel so fortunate to have had so many friends and colleagues come out to support me. I presented the bioinformatics approach I employed to identify putative venom allergen-like proteins from lots of flatworm transcriptomes and why I think understanding their diversification will be helpful...","categories": ["jekyll","update"],
+        "excerpt":"I presented the bioinformatics approach I employed to identify putative venom allergen-like proteins from lots of flatworm transcriptomes and why I think understanding their diversification will be helpful for understanding the evolution of parasitism, particularly in this group. My hope is my pipeline may be helpful to others interested in...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/successfully-defended-MS/",
         "teaser":"http://localhost:4000/assets/images/paradise-ridge.png"},{
@@ -25,7 +25,7 @@ var store = [{
         "url": "http://localhost:4000/jekyll/update/made-it-to-moscow/",
         "teaser":"http://localhost:4000/assets/images/paradise-ridge.png"},{
         "title": "Data Carpentries training",
-        "excerpt":"I just completed the two-day instructor training through The Carpentries alongside a few other colleages from Bioinformatics and Computational Biology. It was so refreshing to talk pedagogy. I’m excited about helping the University of Idaho host a few Software and Data Carpentries workshops in the Spring to help empower others...","categories": ["jekyll","update"],
+        "excerpt":"16-hour pedagogical training down! I just completed the two-day instructor training through The Carpentries alongside a few other colleages from Bioinformatics and Computational Biology. It was so refreshing to talk pedagogy. I’m excited about helping the University of Idaho host a few Software and Data Carpentries workshops in the Spring...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/data-carpentries-training/",
         "teaser":"http://localhost:4000/assets/images/paradise-ridge.png"}]
